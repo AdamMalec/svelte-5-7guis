9b528e313a02c5e618c892b8bfa8a01114a8894b
Adam Malec <add.malec@gmail.com>
2025-06-02 15:50:14 +0300

    feat(package): add open-props devDependencies to project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>7GUIs</h1>
+<p><a href="https://eugenkiss.github.io/7guis/tasks">Seven tasks</a> that represent typical challenges in GUI programming.</p>
