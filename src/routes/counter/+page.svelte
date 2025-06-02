4002e774f647f6b688ffc0dbf0a9d1c81be8067d
Adam Malec <add.malec@gmail.com>
2025-06-02 16:40:18 +0300

    feat: do counter task

---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let count = $state(0);
+</script>
+
+<h1>Counter</h1>
+<div class="flex-gap">
+  <input type="text" bind:value={count} disabled>
+  <button onclick="{() => {count += 1}}">Count</button>
+</div>
+
+
+<style>
+  button {
+    box-shadow: none;
+  }
+</style>
